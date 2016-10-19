d1ab9b913268ae3f226f5ecdd47a5f34edd7fc88
Daniel Singer <daniel@chilblane.com>
2016-10-19 12:01:17 -0700

    redo project structure; add watch/connect/etc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,56 @@
 module.exports = function(grunt) {
+    require('time-grunt')(grunt);
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
-        browserify: {
-            options: {
-                browserifyOptions: {
-                    transform: ['babelify']
-                }
+        watch: {
+            stylus: {
+                files: ['src/stylus/**/*.styl'],
+                tasks: ['stylus:dist']
             },
-            build: {
-                files: {
-                    'dist/scripts.js': ['assets/js/**/*.js']
-                }
-            }
-        },
-
-        copy: {
-            main: {
-                expand: true,
-                dot: true,
-                cwd: 'assets/static',
-                src: '**',
-                dest: 'dist/'
-            }
-        },
-
-        jekyll: {
-            serve: {
+            browserify: {
+                files: ['src/js/**/*.js'],
+                tasks: ['browserify:dist']
+            },
+            copy: {
+                files: ['src/static/**'],
+                tasks: ['copy:dist']
+            },
+            jekyll: {
+                files: ['src/pages/**/*.html'],
+                tasks: ['jekyll:dist']
+            },
+            livereload: {
                 options: {
-                    serve: true
-                }
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    'src/stylus/**/*.styl',
+                    'src/js/**/*.js',
+                    'src/static/**',
+                    '**/*.html'
+                ]
             }
         },
-
+        connect: {
+            options: {
+                port: 9000,
+                hostname: 'localhost',
+                base: 'dist',
+                livereload: 35729
+            },
+            server: {
+            }
+        },
+        clean: {
+            dist: {
+                files: [{
+                    src: [
+                        'dist/*'
+                    ]
+                }]
+            }
+        },
         stylus: {
             options: {
                 compress: false,
@@ -43,26 +61,58 @@ module.exports = function(grunt) {
                     "node_modules/rupture"
                 ]
             },
-
-            development: {
+            dist: {
                 files: [{
                     expand: true,
-                    cwd: 'assets/stylus/',
+                    cwd: 'src/stylus/',
                     src: ['styles.styl'],
-                    dest: 'dist/',
+                    dest: 'dist/assets/',
                     ext:'.css'
                 }]
             }
+        },
+        browserify: {
+            options: {
+                browserifyOptions: {
+                    transform: ['babelify']
+                }
+            },
+            dist: {
+                files: {
+                    'dist/assets/scripts.js': ['src/js/**/*.js']
+                }
+            }
+        },
+        copy: {
+            dist: {
+                expand: true,
+                dot: true,
+                cwd: 'src/static',
+                src: '**',
+                dest: 'dist/assets/'
+            }
+        },
+        jekyll: {
+            dist: {}
         }
+
     });
 
-    grunt.registerTask('build', function() {
+    grunt.registerTask('serve', [
+        'connect:server',
+        'watch'
+    ]);
 
-        var tasks = [];
-        tasks.push('copy', 'stylus:development', 'jekyll:serve');
-        grunt.task.run(tasks);
-    });
+    grunt.registerTask('build', [
+        'clean:dist',
+        'jekyll:dist',
+        'stylus:dist',
+        'browserify:dist',
+        'copy:dist'
+    ]);
 
-    grunt.registerTask('default', ['build']);
+    grunt.registerTask('develop', ['build', 'serve']);
+
+    grunt.registerTask('default', ['develop']);
 
 };
