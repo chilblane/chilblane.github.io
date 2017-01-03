b5f4e3eb3a3cb2466d3820989bbea3ae3653fdff
Daniel <daniel@chilblane.com>
2017-01-02 17:09:17 -0700

    mobile navigation

--- assets/scripts.js
+++ assets/scripts.js
@@ -10224,11 +10224,23 @@ return jQuery;
 (function (window, document) {
     var $ = require('jquery');
 
+    var $body = $('body');
+    var navMenuBtn = $('.js-nav-menu-btn');
+    var mobileNavList = $('.js-mobile-nav-list li');
+    var mobileNavLinks = $('.js-mobile-nav-list li a');
+
+    function init() {
+        navMenuBtn.on('click', toggleMenu);
+        mobileNavLinks.on('click', toggleMenu);
+    }
+
+    function toggleMenu() {
+        $body.toggleClass('js-nav-open');
+    }
+
     if (~['complete', 'interactive'].indexOf(document.readyState)) {
         init();
     }
-
-    function init() {}
 })(window, document);
 
 },{"jquery":1}]},{},[2]);
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,21 @@
 (function(window, document) {
     var $ = require('jquery');
 
-    if( ~['complete', 'interactive'].indexOf(document.readyState)) {
-        init();
-    }
+    var $body = $('body');
+    var navMenuBtn = $('.js-nav-menu-btn');
+    var mobileNavList = $('.js-mobile-nav-list li');
+    var mobileNavLinks = $('.js-mobile-nav-list li a');
 
     function init() {
+        navMenuBtn.on('click', toggleMenu);
+        mobileNavLinks.on('click', toggleMenu);
+    }
+
+    function toggleMenu() {
+        $body.toggleClass('js-nav-open');
+    }
+
+    if( ~['complete', 'interactive'].indexOf(document.readyState)) {
+        init();
     }
 })(window, document);
