de7b93a2a6bdb58c3c347c692b2811ba87a2f6c0
Daniel <daniel@chilblane.com>
2017-05-19 14:47:24 -0700

    resume added

--- src/app/components/Resume.jsx
+++ src/app/components/Resume.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+const resumePath = 'https://github.com/chilblane/chilblane-resume/raw/master/resume.pdf';
+const iframeSrc = 'https://docs.google.com/gview?url=' + resumePath + '&embedded=true';
+
 export default function () {
   return (
-    <div id='resume'>
-      <h2>Resume</h2>
+    <div id='resume' className="embed-container embed-container--paper">
+      <iframe
+        src={iframeSrc}
+        title='Resume for Daniel Singer'
+        frameBorder='0' />
     </div>
   );
 };
