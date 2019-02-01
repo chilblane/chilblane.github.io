ceed21cabcf304bf626b16c8613c408a0ee67333
Daniel Singer <daniel@chilblane.com>
2019-01-31 23:15:16 -0700

    reboot site as a new CRA with gh-pages support

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # chilblane.github.io
+
 My personal website in development.
-
-## Acknowledgments
-
-A significant thanks to [Max Korp](https://github.com/maxkorp) for helping me with the React foundation of this website, as well as some other issues.
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <p>Daniel Singer // chilblane</p>
+                <p>
+                    There&rsquo;s nothing here right now, but that&rsquo;s
+                    subject to change!
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
